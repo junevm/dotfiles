0caf211a452177355c1cf0d41dc2f85528a55a49
dragonrider <62926341+junevm@users.noreply.github.com>
2026-05-27 11:29:20 +0530

    chezmoi - update May 27, 2026 11:29 AM

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,7 +13,6 @@ fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
 
 # Environment variables
-set -gx HOMEBREW_NO_AUTO_UPDATE 1
 set -Ux CHROME_EXECUTABLE "/usr/bin/brave-browser"
 set -gx SSH_AUTH_SOCK "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock" # if bitwarden installed via flatpak
 # set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock" # if bitwarden installed via native package manager
@@ -21,13 +20,6 @@ set -gx SSH_AUTH_SOCK "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-
 # Allow local docker containers to connect to X server
 if command -q xhost
 	xhost +local:docker &>/dev/null
-end
-
-# Initialize Homebrew
-if command -q brew
-	eval "$(brew shellenv)"
-else if test -x /home/linuxbrew/.linuxbrew/bin/brew
-	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 end
 
 # Initialize tools
